e0a469361c69ecbd0de5b5c4f917290a5a46f87c
nothal <nonth@live.com>
2019-03-02 23:35:51 +0700

    fix issue from initial upload
    the initial commit has some typo, this change correct that.

--- obj.js
+++ obj.js
@@ -1,1 +1,82 @@
-const a=z=>{let b;if(z.tag)b=document.createElement(z.tag);if(z.id)b.id=z.id;if(z.classes)for(c of z.classes)b.classList.add(c);if(z.type)b.type=z.type;if(z.name)b.name=z.name;if(z.attributes)for(c of z.attributes)b.setAttribute(c.name,c.value);if(z.text)b.innerHTML=z.text;if(z.children)for(d of z.children)b.appendChild(el(d));return b;},an=a,all=s=>{let r=document.querySelectorAll(s);if(r.length==1)return r[0];else return r;},the=s=>{let r=document.querySelector(s);return r;},make=s=>{let c=s.split(".");s=c.shift();s=s.split("#");let i=s[1];let t=s[0];return a({tag:t,id:i,classes:c});};Node.prototype.has=Node.prototype.appendChild;NodeList.prototype.have=function(c){let k=Array();for(e of this){k.push(c.cloneNode(true));}for(var i=0;i<k.length;i++){this[i].appendChild(k[i]);}return this;};Node.prototype.in=function(e){e.appendChild(this);};HTMLElement.prototype.is=function(){for(c of arguments)this.classList.add(c);}
+Node.prototype.in = function(e) {
+  e.appendChild(this) ;
+};
+HTMLElement.prototype.is = function() {
+  for(c of arguments) {
+    this.classList.add(c);
+  }
+};
+NodeList.prototype.are = function() {
+  let cs = Array() ;
+  for(g of arguments) {
+    cs.push(g);
+  }
+  for(q of this) {
+    for(g of cs) {
+      q.is(g);
+    }
+  }
+};
+Node.prototype.has = Node.prototype.appendChild;
+NodeList.prototype.have = function(c) {
+  let k = Array() ;
+  for(e of this) {
+    k.push(c.cloneNode(true));
+  }
+  for(var i = 0;i<k.length;i++) {
+    this[i].has(k[i]);
+  }
+  return this;
+};
+const a = function(z) {
+  let b = document.createElement(z.tag);
+  if(z.id) {
+    b.id = z.id;
+  }
+  if(z.classes) {
+    for(c of z.classes) {
+      b.is(c);
+    }
+  }
+  if(z.type) {
+    b.type = z.type;
+  }
+  if(z.name) {
+    b.name = z.name;
+  }
+  if(z.attributes) {
+    for(c of z.attributes) {
+      b.setAttribute(c.name, c.value);
+    }
+  }
+  if(z.text) {
+    b.innerHTML = z.text;
+  }
+  if(z.children) {
+    for(d of z.children) {
+      b.has(a(d));
+    }
+  }
+  return b;
+},
+an = a,
+all = function(s) {
+  let r = document.querySelectorAll(s) ;
+  if(r.length == 1) {
+    return r[0];
+  }else {
+    return r;
+  }
+},
+the = function(s) {
+  let r = document.querySelector(s) ;
+  return r;
+},
+make = function(s) {
+  let c = s.split(".");
+  s = c.shift();
+  s = s.split("#");
+  let i = s[1];
+  let t = s[0];
+  return a({tag:t, id:i, classes:c}) ;
+};
